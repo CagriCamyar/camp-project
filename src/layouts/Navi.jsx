200a3c8f84b250e3e63624ee5f4b6f43cb67d7f0
çağrı çamyar <cagri-95@hotmail.com>
2024-10-08 23:31:16 +0300

    SignedIn SignedOut

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,27 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import CartSummary from "./CartSummary";
-import {MenuMenu,MenuItem,Button,Dropdown,Menu,Container} from 'semantic-ui-react'
+import {
+  MenuMenu,
+  MenuItem,
+  Menu,
+  Container,
+} from "semantic-ui-react";
+import SignedOut from "./SignedOut";
+import SignedIn from "./SignedIn";
 
 export default function Navi() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+
+  function handleSignOut(params) {  
+  setIsAuthenticated(false);
+}
+
+function handleSignIn(params) {
+  setIsAuthenticated(true);
+}
+
   return (
     <div>
       <Menu inverted fixed="top">
         <Container>
-        <MenuItem
-          name="home"
-        />
-        <MenuItem
-          name="messages"       
-        />
+          <MenuItem name="home" />
+          <MenuItem name="messages" />
 
-        <MenuMenu position="right">
-        <CartSummary/>
-          <MenuItem>
-            <Button primary>Sign Up</Button>
-          </MenuItem>
-        </MenuMenu>
+          <MenuMenu position="right">
+            <CartSummary />
+            {isAuthenticated?<SignedIn signOut={handleSignOut} bisey="1" />:<SignedOut signIn={handleSignIn}/>}          
+          </MenuMenu>
         </Container>
-       
       </Menu>{" "}
     </div>
   );
