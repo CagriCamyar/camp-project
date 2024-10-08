f22bf1b12d2d826744a00a66a568c453ac66fec6
çağrı çamyar <cagri-95@hotmail.com>
2024-10-08 22:28:11 +0300

    ReactCamp1

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import {
+  TableRow,
+  TableHeaderCell,
+  TableHeader,
+  TableFooter,
+  TableCell,
+  TableBody,
+  MenuItem,
+  Icon,
+  Label,
+  Menu,
+  Table,
+} from "semantic-ui-react";
+import ProductService from "../services/productService";
+export default function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    let productService = new ProductService();
+    productService.getProducts().then((result) => setProducts(result.data.data));
+  },[]);
+
+  return (
+    <div>
+      <Table celled>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderCell>Urun Adi</TableHeaderCell>
+            <TableHeaderCell>Birim Fiyati</TableHeaderCell>
+            <TableHeaderCell>Stok Adedi</TableHeaderCell>
+            <TableHeaderCell>Aciklama</TableHeaderCell>
+            <TableHeaderCell>Kategori</TableHeaderCell>
+          </TableRow>
+        </TableHeader>
+
+        <Table.Body>
+          {products.map((product) => (
+            <Table.Row key={product.id}>
+              <Table.Cell>{product.productName}</Table.Cell>
+              <Table.Cell>{product.unitPrice}</Table.Cell>
+              <Table.Cell>{product.unitsInStock}</Table.Cell>
+              <Table.Cell>{product.quantityPerUnit}</Table.Cell>
+              <Table.Cell>{product.category.categoryName}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+
+        <TableFooter>
+          <TableRow>
+            <TableHeaderCell colSpan="3">
+              <Menu floated="right" pagination>
+                <MenuItem as="a" icon>
+                  <Icon name="chevron left" />
+                </MenuItem>
+                <MenuItem as="a">1</MenuItem>
+                <MenuItem as="a">2</MenuItem>
+                <MenuItem as="a">3</MenuItem>
+                <MenuItem as="a">4</MenuItem>
+                <MenuItem as="a" icon>
+                  <Icon name="chevron right" />
+                </MenuItem>
+              </Menu>
+            </TableHeaderCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </div>
+  );
+}
